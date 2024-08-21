8f87d2294f40f70e5591842457eae66f75c3b482
Mahmudjanov <makhmudjanovmuhammadsodiq@gmail.com>
2024-08-21 15:02:54 +0500

    hello

--- main/urls.py
+++ main/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
 from.views import *
+
+
+
+urlpatterns = [
+    path('get-product/', get_product),
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from. serializer import *
 
-# Create your views here.
+
+
+@api_view(['GET'])
+def get_product(request):
+    roduct = Product.objects.last()
+    serialized_data = ProductSerializer(roduct).data 
+    return Response(serialized_data)
